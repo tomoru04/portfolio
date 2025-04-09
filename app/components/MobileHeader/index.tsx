4ce639dabb8bc6e3191f56a1ace10338b5a36722
tomoru04 <shodasandesuyo@icloud.com>
2025-04-10 04:04:28 +0900

    modify: changed a tag into Link tag

--- app/components/MobileHeader/index.tsx
+++ app/components/MobileHeader/index.tsx
@@ -3,6 +3,7 @@ import { FC } from 'react'
 import * as styles from './mobileHeader.css'
 import { NavigationItems } from '../NavigationItems'
 import React from 'react'
+import Link from 'next/link'
 import { HamburgerMenuIcon } from '@radix-ui/react-icons'
 export const MobileHeader: FC = () => {
   const [open, setOpen] = React.useState(false)
@@ -12,9 +13,9 @@ export const MobileHeader: FC = () => {
   return (
     <div className={styles.container}>
       <div className={styles.header}>
-        <a href='/' className={styles.name}>
+        <Link href='/' className={styles.name}>
           Tomoya Shoda
-        </a>
+        </Link>
         <button onClick={toggleNavigationBarOpen} className={styles.button}>
           <HamburgerMenuIcon width={'30px'} height={'30px'} className={styles.icon} />
         </button>
