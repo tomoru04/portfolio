219b2d04a673010f24468e877dd222eca22dfe2c
tomoru04 <shodasandesuyo@icloud.com>
2026-02-08 05:44:07 +0900

    fixed: changed filepath into relative one so as to refer to it in the Vercel server

--- app/hooks/getAchievementsData.ts
+++ app/hooks/getAchievementsData.ts
@@ -1,8 +1,11 @@
 import { readFile } from 'fs/promises'
 import path from 'path'
 import { AchievementsData } from '@/scripts'
+
 export const getAchievementsData = async () => {
-  const files = await readFile(path.join(__dirname, '../../../../data/blog.json'))
+  const filePath = path.join(process.cwd(), "data", "blog.json");
+  const files = await readFile(filePath, "utf-8");
+
   const achievementsData: Record<string, AchievementsData[]> = JSON.parse(files.toString())
   return { achievementsData }
 }
